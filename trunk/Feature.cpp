c0a098694bcbf89d9f4fd2be26565c5ac8a3854f
deepest1@gmail.com <deepest1@gmail.com@9d8a622a-af3f-11dd-8748-9355f1163679>
2008-11-13 21:34:59 +0000

    Pokusaj spajanja BaseFeature i Feature u jedno, nema sintaksnih errora, no nije testirano dal radi.
    Bonaci testiraj to, evo ti commitam za to i ProjektCV_FJA_ZA_TEST.cpp u kojem imas test dio koji bi trebo moc to testirat.
    
    Testiraj obavezno!!!!!!!!!!!!!!!!
    
    
    Ivan.

--- trunk/Feature.cpp
+++ trunk/Feature.cpp
@@ -12,28 +12,68 @@ Feature::~Feature(void)
 {
 }
 
-Feature::Feature(int tBase, int tX, int tY, float tScale)
+Feature::Feature(int w,int h,vector<pair<int,int> > zb,vector<pair<int,int> > od, int tX, int tY, float tScale, float tWeight)
 {
-	baseRb=tBase;
+	width=w;
+	height=h;
+	add=zb;
+	subtract=od;
+
 	x=tX;
 	y=tY;
-	scale=tScale;	
+	scale=tScale;
+	width=tWeight;
 }
 
 vector<Feature> Feature::generateAll(int x,int y, int width, int height, int step, float scaleFactor)
 {
 	vector<Feature> ret;
 	
-	vector<BaseFeature> &bf=BaseFeature::allBaseFeatures;
+	vector<Feature> &bf=allBaseFeatures;
 	
 	float f;
 	for(int i,j,k=0;k<bf.size();k++)
 		for(f=1;f*bf[k].width<=width && f*bf[k].height<=height;f*=scaleFactor)
 			for(i=0;(int)(i+f*bf[k].width)<width;i+=step)
 				for(j=0;j+(int)(f*bf[k].height)<height;j+=step) {
-					Feature tmp(k,i,j,f);
+					Feature tmp(bf[k].width,bf[k].height,bf[k].add,bf[k].subtract,i,j,f,-1);  // weight postavljen na -1
 					ret.push_back(tmp);
 				}
 	
 	return ret;
 }
+
+void Feature::loadBaseFeatures(string file)
+{
+	FILE *in=fopen(file.c_str(),"r");
+	
+	allBaseFeatures.clear();
+
+	int n;
+	fscanf(in,"%d",&n);
+	
+	for(int a,b,m,w,h,j,i=0;i<n;i++) {
+		fscanf(in,"%d %d",&w,&h);
+
+		vector<pair<int,int> > zb;
+		fscanf(in,"%d",&m);
+		for(j=0;j<m;j++) {
+			fscanf(in,"%d %d",&a,&b);
+			zb.push_back(make_pair(a,b));
+		}
+
+		vector<pair<int,int> > od;
+		fscanf(in,"%d",&m);
+		for(j=0;j<m;j++) {
+			fscanf(in,"%d %d",&a,&b);
+			od.push_back(make_pair(a,b));
+		}
+
+		allBaseFeatures.push_back(Feature(w,h,zb,od,-1,-1,-1,-1));
+	}
+
+	fclose(in);
+}
+
+// izgleda da je ovo nuzno da bi vektor allBaseFeatuers bio i definiran, a ne samo deklaniran
+vector<Feature> Feature::allBaseFeatures;
